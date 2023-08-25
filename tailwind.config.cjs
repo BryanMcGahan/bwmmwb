c2475e19a217034d7caa754a28f6a9fc882a85b4
BryanMcGahan <mcgahan72@outlook.com>
2023-08-24 21:03:30 -0500

    idk

---
+++ tailwind.config.cjs
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    colors: {
+      "sumilnk0": "#1F1F28",
+      "bgsh":"#16161D",
+      "fg": "#DCD7BA",
+      "hbc": "#2D4F67",
+      "hfc": "#C8C093",
+      "bold": "#72A7BC",
+      "badge": "#1F1F28",
+      "peachRed": "#C34043",
+      "waveBlue": "#2D4F67",
+      "katanaGray": "#717C7C",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
